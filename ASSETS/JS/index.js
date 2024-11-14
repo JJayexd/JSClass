ce1775313d0885be5c2be57e586fe535ab62d237
JJayexd <kontakt.emilbj@gmail.com>
2024-11-14 08:58:15 +0100

    Class Car

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -1,4 +1,4 @@
-class VisitCard {
+export class VisitCard {
     constructor(firstname, surname, email, birthdate, position, gender) {
         this.firstname = firstname;
         this.surname = surname;
@@ -9,7 +9,6 @@ class VisitCard {
     }
 
     present() {
-        // console.log(`${this.firstname} ${this.surname}, ${this.email}, ${this.birthdate}, ${this.position}, ${this.gender}`);
         console.log(`Fornavn: ${this.firstname}`);
         console.log(`Efternavn: ${this.surname}`);
         console.log(`Email: ${this.email}`);
@@ -23,4 +22,34 @@ const person1 = new VisitCard('Jens', 'Jørgen', 'jensj@gmail.com', '4/06/1994',
 const person2 = new VisitCard('Søren', 'Jørgensen', 'sørenbo@gmail.com', '7/12/2000', 'Tømrer', 'Male');
 const person3 = new VisitCard('Emil', 'Jørgensen', 'kontakt.emilbj@gmail.com', '30/06/2003', 'Webudvikler', 'Male');
 
-person1.present();
+// person1.present();
+
+export class Car {
+    constructor(brand, model, propellant, range, color, year, kilometers, desc, price) {
+        this.brand = brand
+        this.model = model
+        this.propellant = propellant
+        this.range = range
+        this.color = color
+        this.year = year
+        this.kilometers = kilometers
+        this.desc = desc
+        this.price = price
+    }
+
+    present () {
+        console.log(`Brand: ${this.brand}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Propellant: ${this.propellant}`);
+        console.log(`Range: ${this.range}`);
+        console.log(`Color: ${this.color}`);
+        console.log(`Year: ${this.year}`);
+        console.log(`Kilometers: ${this.kilometers}`);
+        console.log(`Description: ${this.desc}`);
+        console.log(`Price: ${this.price}`);
+    }
+}
+
+const car1 = new Car('Tesla', 'Model 3', 'Electric', '?', 'White', '2020', '10000', 'This is a Tesla Model 3', '100000 USD');
+
+car1.present();
